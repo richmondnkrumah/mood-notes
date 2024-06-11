587a71b29a9e7a7c6c6d60767aead7b45c62425d
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-11 12:07:54 +0000

    added emoji default constants

---
+++ constants/Emojis.ts
@@ -0,0 +1,36 @@
+type EMOJI = {
+  name: string,
+  color: string
+}
+
+export const Emojis: EMOJI[] = [{
+  name: "Happy",
+  color: "#FFEBEB"
+},
+{
+  name: "Ecstatic",
+  color: "#FFE1C2"
+},
+{
+  name: "Frustrated",
+  color: "#D6FFD6"
+},{
+  name: "Angry",
+  color: "#FFEBD6"
+
+},{
+  name: "Neutral",
+  color: "#D6D6FF"
+},{
+  name: "Annoyed",
+  color: "#EDD6FF"
+},{
+  name: "Furious",
+  color: "#EDD6FF"
+},{
+  name: "Displeased",
+  color: "#EDD6FF"
+},{
+  name: "Sad",
+  color: "#EDD6FF"
+},]
