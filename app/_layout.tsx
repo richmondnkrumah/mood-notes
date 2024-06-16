9eb8aeecaa8b8ced7dfdbb253cee7b6a8eae65c1
richmondnkrumah <richmondnkrumah04@gmail.com>
2024-06-16 17:53:26 +0000

    integrated tamagui into the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,29 +3,46 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import DEFAULT_TAGS from '@/constants/tags';
 import storeData from '@/utils/storeData';
-
+import { ToastProvider, ToastViewport } from '@tamagui/toast'
+import { tamaguiConfig } from '../tamagui.config'
+import { TamaguiProvider } from 'tamagui'
+import { useFonts } from 'expo-font'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  
-
-  // useEffect(() => {
-  //   if (loaded) {
-  //     SplashScreen.hideAsync();
-  //   }
-  // }, [loaded]);
-
-  // if (!loaded) {
-  //   return null;
-  // }
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  })
   const router = useRouter()
+
   useEffect(() => {
-    storeData(DEFAULT_TAGS,DEFAULT_TAGS.key,"object")
-    router.push('/home')
-    },[])
+    const clear = async() => {
+      await AsyncStorage.removeItem('note')
+      await AsyncStorage.removeItem('notes')
+    }
+    // clear()
+    if (loaded) {
+      SplashScreen.hideAsync();
+      storeData(DEFAULT_TAGS, DEFAULT_TAGS.key, "object")
+      router.push('/home')
+
+
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
-      <Stack screenOptions={{headerShown:false}}/>
+    <TamaguiProvider config={tamaguiConfig}>
+      <ToastProvider>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ToastProvider>
+    </TamaguiProvider>
+
   );
 }
